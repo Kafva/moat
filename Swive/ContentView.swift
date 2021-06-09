6eb2b3793df031bd757eb3da7ce7d6382655aeef
Kafva <Martensson.Jonas98@gmail.com>
2021-06-09 16:09:14 +0200

    -

---
+++ Swive/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Swive
+//
+//  Created by Jonas Mårtensson on 2021-06-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("XDDDD bolder").bold().strikethrough();
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
