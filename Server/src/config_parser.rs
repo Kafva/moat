18e6314f2cfb0639c485e0cacc7cc4f095539511
Kafva <Martensson.Jonas98@gmail.com>
2021-06-14 23:01:34 +0200

    -

---
+++ Server/src/config_parser.rs
@@ -0,0 +1,9 @@
+use std::fs;
+
+struct Config {
+    cache_path: String,
+}
+
+fn get_config(config_path: String) -> Result<Config> {
+        
+}
--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,11 +1,48 @@
 #[macro_use] extern crate rocket;
+extern crate clap;
+use rocket::Rocket;
+use rocket::Build;
+use clap::{AppSettings, Clap};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, server"
+mod db_parser;
+mod routes;
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Kafva <https://github.com/Kafva>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Sets a custom config file
+    #[clap(short, long, default_value = "./conf/server.conf")]
+    config: String,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: String,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
 }
 
+// The launch attribute generates a main() function 
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn rocket() -> Rocket<Build> {
+    
+    let opts: Opts = Opts::parse();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    println!("Value for config: {}", opts.config);
+    println!("Using input file: {}", opts.input);
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    
+    // Start the server
+    rocket::build().mount("/", routes![routes::index])
 }
