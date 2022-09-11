fda2ab3bfad4015283ae9802462fb991b0cc4efb
Kafva <jonas@kafva.one>
2022-09-11 13:05:48 +0200

    -

--- Swive/Config.swift
+++ Swive/Config.swift
@@ -1,9 +1,27 @@
 import SwiftUI
 
-/// Sprites ///
+/***** Colors *******/
+let BKG_GRADIENT = Gradient(colors: [
+    Color(hex: "#606c88"),
+    Color(hex: "#3f4c6b")
+]); 
+
+/***** Sprites *******/
+let BASE_NODE_NAME = "node";
+
+/// No new sprites will be spawned when the total number
+/// reaches this value
 let MAX_SPRITE_COUNT = 14;
+let INITIAL_SPAWN_COUNT = 3;
+
+/// Configure in accordance with the sprite sheet being used 
 let COLUMN_COUNT = 6;
 let ROW_COUNT = 1;
-let SPRITE_SHEET = "pika-h.png";
 let SPRITE_SIZE = CGSize(width:37*2, height:47*2);
-let BASE_NODE_NAME = "node"
+let SPRITE_SHEET = "pika-h.png";
+
+
+/// The interval (in seconds) to cycle frames in a sprite object
+let SPRITE_NEW_FRAME_INTERVAL = 0.5;
+let SPRITE_SPAWN_INTERVAL: Double = 1;
+
--- Swive/SpriteScene.swift
+++ Swive/SpriteScene.swift
@@ -1,7 +1,10 @@
 import SpriteKit
+import SwiftUI;
 
 class SpriteScene: SKScene {
     
+    @State var spawn: Bool = true;
+
     var sprites: [SKSpriteNode] = [];
     var nodeCount: uint = 0;
 
@@ -14,145 +17,27 @@ class SpriteScene: SKScene {
         spriteSize: SPRITE_SIZE
     );
     
+    var spawnTimer: Timer?;
+
     private func setupTimers(){
        
-       Timer.scheduledTimer(
-           timeInterval: 1, 
+       self.spawnTimer = Timer.scheduledTimer(
+           timeInterval: SPRITE_SPAWN_INTERVAL, 
            target: self, 
-           selector: #selector(addRandomSprite), 
+           selector: #selector(addSpriteAtRandomPos), 
            userInfo: nil,
            repeats: true
         ); 
        
        Timer.scheduledTimer(
-           timeInterval: 0.5, 
+           timeInterval: SPRITE_NEW_FRAME_INTERVAL, 
            target: self, 
            selector: #selector(cycleSprites), 
            userInfo: nil,
            repeats: true
-        ); 
-    }
-
-    /// The position is the position of the sprite in the parent container
-    /// If no position is provided a random position will be chosen
-    /// No sprite will be added if the limit has been reached
-    func addSprite(position: CGPoint) {
-        
-        guard nodeCount < MAX_SPRITE_COUNT else { 
-            #if DEBUG
-                NSLog("Limit reached (\(nodeCount)/\(MAX_SPRITE_COUNT)) -- not adding a new sprite");
-            #endif
-            return; 
-        }
-
-        do {
-            let sprite = try self.sheet.getSprite(
-                columnIndex: Int.random(in: 0...COLUMN_COUNT-1), 
-                rowIndex: Int.random(in: 0...ROW_COUNT-1),
-                name: "\(BASE_NODE_NAME)\(self.nodeCount)"
-            );
-            self.nodeCount+=1
-
-            sprite.position = position;
-            NSLog("\(sprite.position) manual"); 
-            
-            self.sprites.append(sprite);
-            self.addChild(sprite);
-            
-            // Pick a point to move to which will have each sprite move
-            // towards the upper right corner along the same line 
-            let targetPos = CGPoint(
-                x: self.size.width + position.x,
-                y: self.size.height + position.y
-            )
-
-            let actions = SKAction.sequence([
-                SKAction.fadeIn(withDuration: 1),
-                SKAction.move(to: targetPos, duration: 5),
-                SKAction.fadeAlpha(to: 0, duration: 1),
-            ])
-
-            sprite.run(actions, completion: {
-                sprite.removeFromParent()
-                if self.nodeCount > 0 { 
-                    self.nodeCount-=1;
-                }
-            });
-        }
-        catch { NSLog("\(error)"); }
-
+       ); 
     }
     
-    @objc func addRandomSprite() {
-        guard nodeCount < MAX_SPRITE_COUNT else { 
-            #if DEBUG
-                NSLog("Limit reached (\(nodeCount)/\(MAX_SPRITE_COUNT)) -- not adding a new sprite");
-            #endif
-            return; 
-        }
-        
-        let spawnX = CGFloat.random( in: 0...(self.size.width  / 2) );
-
-        // Default spawn locations
-        let position = CGPoint(
-            x: arc4random() % 2 == 0 ? 
-                spawnX : -1 * spawnX,
-            y: -1 * CGFloat.random( in: 0...(self.size.height / 4) )
-        )
-
-        do {
-            let sprite = try self.sheet.getSprite(
-                columnIndex: Int.random(in: 0...COLUMN_COUNT-1), 
-                rowIndex: Int.random(in: 0...ROW_COUNT-1),
-                name: "\(BASE_NODE_NAME)\(self.nodeCount)"
-            );
-            self.nodeCount+=1
-
-            sprite.position = position;
-            
-            self.sprites.append(sprite);
-            NSLog("\(sprite.position) rand()");
-            self.addChild(sprite);
-            
-            // Pick a point to move to which will have each sprite move
-            // towards the upper right corner along the same line 
-            
-            var posX = self.size.width  + position.x;
-            var posY = self.size.height + position.y;
-            let k = abs(position.y/position.x);
-
-            // Ensure that every sprite reaches the end of the screen
-            while posX < self.size.width || posY < self.size.height {
-                posY+=k;
-                posX+=k;
-            }
-
-            let targetPos = CGPoint(
-                //x: self.size.width  + position.x,
-                //y: self.size.height + position.y
-                x: posX,
-                y: posY
-            )
-
-            NSLog("\(sprite.position) fix()");
-
-            let actions = SKAction.sequence([
-                SKAction.fadeIn(withDuration: 1),
-                SKAction.move(to: targetPos, duration: 5),
-                SKAction.fadeAlpha(to: 0, duration: 1),
-            ])
-
-            sprite.run(actions, completion: {
-                sprite.removeFromParent()
-                if self.nodeCount > 0 { 
-                    self.nodeCount-=1;
-                }
-            });
-        }
-        catch { NSLog("\(error)"); }
-
-    }
-
     /// Go through all SKSpriteNode objects in the scene and update their
     /// sprite image.
     /// The @objc label is needed for functions that are ran using a timer
@@ -169,7 +54,79 @@ class SpriteScene: SKScene {
                 catch { NSLog("\(error)"); }
             }
     }
-    
+
+    @objc func addSpriteAtRandomPos() {
+
+        let spawnX = CGFloat.random( in: 0...(self.size.width  / 1) );
+
+        // Default spawn location calculation
+        let spawnPosition = CGPoint(
+            x: arc4random() % 2 == 0 ? 
+                spawnX : -1 * spawnX,
+            y: -1 * CGFloat.random( in: 0...(self.size.height / 4) )
+        )
+        
+        // Pick a point to move to which will have each sprite move
+        // towards the upper right corner along the same line 
+        var posX = self.size.width  + spawnPosition.x;
+        var posY = self.size.height + spawnPosition.y;
+        let k = abs(spawnPosition.y/spawnPosition.x);
+
+        // Ensure that every sprite reaches the end of the screen
+        while posX < self.size.width || posY < self.size.height {
+            posY+=k;
+            posX+=k;
+        }
+
+        let targetPos = CGPoint(
+            x: posX,
+            y: posY
+        )
+
+        self.addSprite(spawnPosition, targetPos);
+    }
+
+    /// Add a sprite from the provided sprite sheet to the scene at the given position
+    /// the sprite will travel towards the point specfied in the second argument
+    func addSprite(_ spawnPosition: CGPoint, _ targetPos: CGPoint) {
+        
+        guard nodeCount < MAX_SPRITE_COUNT else { 
+            #if DEBUG
+                NSLog("Limit reached (\(nodeCount)/\(MAX_SPRITE_COUNT)) -- not adding a new sprite");
+            #endif
+            return; 
+        }
+
+        do {
+            let sprite = try self.sheet.getSprite(
+                columnIndex: Int.random(in: 0...COLUMN_COUNT-1), 
+                rowIndex: Int.random(in: 0...ROW_COUNT-1),
+                name: "\(BASE_NODE_NAME)\(self.nodeCount)"
+            );
+            self.nodeCount+=1
+
+            sprite.position = spawnPosition;
+            NSLog("Placing new sprite at (\(round(sprite.position.x)),\(round(sprite.position.y)))"); 
+            
+            self.sprites.append(sprite);
+            self.addChild(sprite);
+            
+            let actions = SKAction.sequence([
+                //SKAction.fadeIn(withDuration: 1),
+                SKAction.move(to: targetPos, duration: 5),
+                //SKAction.fadeAlpha(to: 0, duration: 1),
+            ])
+
+            sprite.run(actions, completion: {
+                sprite.removeFromParent()
+                if self.nodeCount > 0 { 
+                    self.nodeCount-=1;
+                }
+            });
+        }
+        catch { NSLog("\(error)"); }
+    }
+
     private func getNodeCount() -> Int{
         var nodeCount=0;
         self.enumerateChildNodes(withName: "\(BASE_NODE_NAME)*") {
@@ -179,20 +136,35 @@ class SpriteScene: SKScene {
         return nodeCount;
     }
 
-    // The `didMove` method is triggered when we enter the scene
+    /// The `didMove` method is triggered when we enter the scene
     override func didMove(to view: SKView) {
+        // Use a clear background to show the gradient behind the scene in the ZStack
+        self.backgroundColor = .clear;
+
         // Setup the global timer for sprite updates
         self.setupTimers();
+        
+        // Immediatelly add a few sprites
+        for _ in 0...INITIAL_SPAWN_COUNT {
+            self.addSpriteAtRandomPos();
+        } 
     }
 
-    // The code to run when a touch event reaches the scene
+    /// The code to run when a touch event reaches the scene
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         // Ensure that the touch event is not nil
         guard let touch = touches.first else { return; }
         let location = touch.location(in: self);
         
-        self.addSprite(position: location);
+        // Pick a point to move to which will have the new sprite move
+        // towards the upper right corner of the screenn 
+        let targetPos = CGPoint(
+            x: self.size.width + location.x,
+            y: self.size.height + location.y
+        )
+        
+        self.addSprite(location, targetPos);
         
         #if DEBUG 
             NSLog("Touched (\(round(location.x)),\(round(location.y)))");
