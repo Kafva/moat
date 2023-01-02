3df402b132a6f1a328381b976e5e5fe8e43e079a
Kafva <jonas@kafva.one>
2023-01-02 20:04:26 +0100

    Actix hello world

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,28 @@
-#[macro_use]
-extern crate rocket;
-use crate::urls_parser::get_muted;
-use clap::Parser;
-use rocket::{Build, Rocket};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-// The `mod` keyword will expand to the contents of the file
-// with the corresponding name
-mod dataguards;
-mod db_parser;
-mod errors;
-mod models;
-mod routes;
-mod urls_parser;
-use crate::db_parser::get_feed_list;
-use models::Config;
-use rocket::shield::{Hsts, Shield};
-
-#[derive(Parser, Debug)]
-#[clap(
-    version = "0.1.0",
-    author = "Kafva <https://github.com/Kafva>",
-    about = "moat server"
-)]
-struct Args {
-    /// Path to newsboat executable
-    #[cfg(target_os = "macos")]
-    #[clap(
-        short,
-        long,
-        default_value = "/opt/homebrew/bin/newsboat",
-        value_parser
-    )]
-    newsboat_path: String,
-
-    #[cfg(target_os = "linux")]
-    #[clap(short, long, default_value = "/usr/bin/newsboat", value_parser)]
-    newsboat_path: String,
-
-    /// Path to newsboat cache.db
-    #[clap(short, long, default_value = "~/.newsboat/cache.db", value_parser)]
-    cache_path: String,
-
-    /// Path to newsboat urls file
-    #[clap(short, long, default_value = "~/.newsboat/urls", value_parser)]
-    urls_path: String,
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
-fn expand_tilde(value: String) -> String {
-    value.replace("~", std::env::var("HOME").unwrap().as_str())
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
 
-// The launch attribute generates a main() function
-#[launch]
-fn rocket() -> Rocket<Build> {
-    let opts: Args = Args::parse();
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
 
-    let config = Config {
-        cache_path: expand_tilde(opts.cache_path),
-        newsboat_path: expand_tilde(opts.newsboat_path),
-        muted_list: get_muted(expand_tilde(opts.urls_path)).unwrap(),
-    };
-
-    // Sanity check, verify that the cache.db exists and has at least one entry
-    let feeds = get_feed_list(&config.cache_path, &config.muted_list);
-    if feeds.is_err() || feeds.unwrap().is_empty() {
-        panic!("cache.db does not exist or is empty")
-    }
-
-    // HTTP headers that should be included in all responses
-    // are configured through 'Shields', here we add 'Strict-Transport-Security'
-    let shield = Shield::default().enable(Hsts::default());
-
-    // Pass the config into the global state of rocket and
-    // start the server with each route mounted at '/'
-    rocket::build()
-        .manage(Config::from(config))
-        .register(
-            "/",
-            catchers![
-                errors::internal_error,
-                errors::unauthorized,
-                errors::not_found,
-                errors::default
-            ],
-        )
-        .mount(
-            "/",
-            routes![
-                routes::feeds,
-                routes::items,
-                routes::reload,
-                routes::unread
-            ],
-        )
-        .attach(shield)
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
